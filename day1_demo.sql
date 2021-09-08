cb7f912012d08dde4d3868e0c90171efecb25f7c
Ben P <polk.ben@gmail.com>
2021-09-08 09:05:36 -0600

    Update postgres demo file

--- day1_demo.sql
+++ day1_demo.sql
@@ -1,5 +1,4 @@
 drop table if exists countries cascade;
-
 create table countries (
         country_code char(2) primary key,
         country_name text unique
@@ -16,3 +15,27 @@ insert into countries (country_code, country_name) values
 INSERT INTO countries VALUES
 	--('uk', 'United Kingdom'),
 	('it', 'Italy');
+
+SELECT * FROM countries;
+
+drop table if exists cities cascade;
+create table cities (
+    name text not null,
+    postal_code varchar(9) check (postal_code <> ''),
+    country_code char(2) references countries,
+    primary key (country_code, postal_code)
+);
+
+insert into cities values ('Toronto', 'M4C1B5', 'ca');
+
+insert into cities values ('Portland', '87200', 'us');
+
+SELECT * FROM cities;
+
+UPDATE cities SET postal_code = '97206' WHERE name = 'Portland' and postal_code = '87200';
+
+SELECT * FROM cities;
+
+SELECT cities.name, cities.postal_code, countries.country_name
+FROM cities
+INNER JOIN countries ON countries.country_code = cities.country_code;
