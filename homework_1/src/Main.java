29c8cc28f54a3d6553d94ce57e61f1551640e5d9
Ben Polk <benjamin.polk@montana.edu>
2021-10-11 15:12:22 -0600

    Add B-Tree tests to Main

--- homework_1/src/Main.java
+++ homework_1/src/Main.java
@@ -7,22 +7,42 @@ public class Main {
     }
 
     public static void testBTree() {
-        BTree<Integer> myTree = new BTree<>(4, 0);
+        BTree<Integer> myTree;
 
-        Random rand = new Random();
-        for (int i = 0; i <= 8; i++) {
-            //myTree.addValue(rand.nextInt(20));
-            myTree.addValue(i);
+        System.out.println("=========SEQUENTIAL TEST=========");
+        //sequential test
+        myTree = new BTree<>(4, 0);
+        for (int i = 0; i <= 40; i++) {
+            myTree.addValue(i, 1);
+            if (i % 2 == 0) myTree.addValue(i, 1);
+            if (i % 3 == 0) myTree.addValue(i, 1);
         }
         myTree.checkTree();
 
-        myTree.addValue(5);
+        System.out.println(System.lineSeparator() + "=========REVERSE SEQUENTIAL TEST=========");
+        //reverse sequential test
+        myTree = new BTree<>(4, 0);
+        for (int i = 40; i >= 0; i--) {
+            myTree.addValue(i, 1);
+            if (i % 2 == 0) myTree.addValue(i, 1);
+            if (i % 3 == 0) myTree.addValue(i, 1);
+        }
         myTree.checkTree();
-        myTree.addValue(5);
+
+        System.out.println(System.lineSeparator() + "=========RANDOM TEST=========");
+        //random test
+        myTree = new BTree<>(4, 0);
+        Random rand = new Random();
+        for (int i =0; i <= 50; i++) {
+            Integer newVal = rand.nextInt(100);
+            while (myTree.search(newVal).getIndexOf(newVal) >= 0) newVal = rand.nextInt(100);
+            myTree.addValue(newVal, 1);
+            if (newVal % 2 == 0) myTree.addValue(newVal, 1);
+            if (newVal % 3 == 0) myTree.addValue(newVal, 1);
+        }
         myTree.checkTree();
 
         System.out.println("done");
-        myTree.checkTree();
     }
 
     public static void runTestDBQueries() {
