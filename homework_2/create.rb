5dce8346ad6c8f35105b8fc2d0f2b7e66c975ad9
Ben P <polk.ben@gmail.com>
2021-10-18 01:24:05 -0600

    Fill out create scripts for food nutrition data

--- homework_2/create.rb
+++ homework_2/create.rb
@@ -1,6 +1,20 @@
+#https://www.tutorialspoint.com/hbase/hbase_create_table.htm
 include Java
 import 'org.apache.hadoop.hbase.client.HTable'
-import 'org.apache.hadoop.hbase.client.Put'
 import 'org.apache.hadoop.hbase.HBaseConfiguration'
+import 'org.apache.hadoop.hbase.HColumnDescriptor'
+import 'org.apache.hadoop.hbase.HTableDescriptor'
+import 'org.apache.hadoop.hbase.client.HBaseAdmin'
+import 'org.apache.hadoop.hbase.TableName'
 
-table = HTable.new(HBaseConfiguration.create, "food_data")
+conf = HBaseConfiguration.create
+admin = HBaseAdmin.new(conf)
+
+tableDescriptor = HTableDescriptor.new(TableName.valueOf("food_data"))
+tableDescriptor.addFamily(HColumnDescriptor.new("display_name"))
+tableDescriptor.addFamily(HColumnDescriptor.new("portion"))
+tableDescriptor.addFamily(HColumnDescriptor.new("type"))
+tableDescriptor.addFamily(HColumnDescriptor.new("nutrition"))
+
+admin.createTable(tableDescriptor)
+print "Table created\n"
