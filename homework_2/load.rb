6cade5b412fc76cb3004218df863020eedc9f783
Ben P <polk.ben@gmail.com>
2021-10-19 01:29:54 -0600

    Rename file to load.rb

--- homework_2/load.rb
+++ homework_2/load.rb
@@ -1,3 +1,4 @@
+#adapted from Dave's notes
 include Java
 
 import 'javax.xml.stream.XMLStreamConstants'
@@ -14,7 +15,7 @@ reader = factory.createXMLStreamReader(java.io.FileInputStream.new("/mnt/Food_Di
 
 document = nil
 buffer = nil
-food_code = nil
+display_name = nil
 count = 0
 
 print "Starting...\n"
@@ -32,34 +33,34 @@ while reader.has_next
 	buffer << reader.text unless buffer.nil?
   when XMLStreamConstants::END_ELEMENT
 	case reader.local_name
-	when 'Food_Code' then
-	  food_code = buffer.join
+#	when 'Food_Code' then
+#	  food_code = buffer.join
 	when 'Display_Name' then
-	  document['display_name'] = buffer.join
-	when 'Portion_Default' then
-	  document['portion:default'] = buffer.join
-	when 'Portion_Amount' then 
-	  document['portion:amount'] = buffer.join
-	when 'Factor' then
-	  document['portion:factor'] = buffer.join
-	when 'Increment' then
-	  document['portion:increment'] = buffer.join
-	when 'Multiplier' then
-	  document['portion:multiplier'] = buffer.join
-	when 'Grains' then
-	  document['type:grains'] = buffer.join
-	when 'Vegetables' then
-	  document['type:vegetables'] = buffer.join
-	when 'Fruits' then
-	  document['type:fruits'] = buffer.join
-	when 'Milk' then
-	  document['type:milk'] = buffer.join
-	when 'Meats' then
-	  document['type:meats'] = buffer.join
-	when 'Soy' then
-	  document['type:soy'] = buffer.join
-	when 'Oils' then
-	  document['type:oils'] = buffer.join
+	  display_name = buffer.join
+#	when 'Portion_Default' then
+#	  document['portion:default'] = buffer.join
+#	when 'Portion_Amount' then 
+#	  document['portion:amount'] = buffer.join
+#	when 'Factor' then
+#	  document['portion:factor'] = buffer.join
+#	when 'Increment' then
+#	  document['portion:increment'] = buffer.join
+#	when 'Multiplier' then
+#	  document['portion:multiplier'] = buffer.join
+#	when 'Grains' then
+#	  document['type:grains'] = buffer.join
+#	when 'Vegetables' then
+#	  document['type:vegetables'] = buffer.join
+#	when 'Fruits' then
+#	  document['type:fruits'] = buffer.join
+#	when 'Milk' then
+#	  document['type:milk'] = buffer.join
+#	when 'Meats' then
+#	  document['type:meats'] = buffer.join
+#	when 'Soy' then
+#	  document['type:soy'] = buffer.join
+#	when 'Oils' then
+#	  document['type:oils'] = buffer.join
 	when 'Solid_Fats' then
 	  document['nutrition:solid_fats'] = buffer.join
 	when 'Added_Sugars' then
@@ -71,24 +72,24 @@ while reader.has_next
 	when 'Saturated_Fats' then
 	  document['nutrition:saturated_fats'] = buffer.join
 	when 'Food_Display_Row'
-	  key = food_code.to_java_bytes
+	  key = display_name.to_java_bytes
 
-	  print "Adding " + document['display_name'] + "(" + food_code + ")...\n"
+	  print "Adding " + display_name + "...\n"
 	  p = Put.new(key)
 	  
-	  p.add(*jbytes("display_name", "", document['display_name']))
-	  p.add(*jbytes("portion", "default", document['portion:default']))
-	  p.add(*jbytes("portion", "amount", document['portion:amount']))
-	  p.add(*jbytes("portion", "factor", document['portion:factor']))
-	  p.add(*jbytes("portion", "increment", document['portion:increment']))
-	  p.add(*jbytes("portion", "multiplier", document['portion:multiplier']))
-	  p.add(*jbytes("type", "grains", document['type:grains']))
-	  p.add(*jbytes("type", "vegetables", document['type:vegetables']))
-	  p.add(*jbytes("type", "fruits", document['type:fruits']))
-	  p.add(*jbytes("type", "milk", document['type:milk']))
-	  p.add(*jbytes("type", "meats", document['type:meats']))
-	  p.add(*jbytes("type", "soy", document['type:soy']))
-	  p.add(*jbytes("type", "oils", document['type:oils']))
+#	  p.add(*jbytes("display_name", "", document['display_name']))
+#	  p.add(*jbytes("portion", "default", document['portion:default']))
+#	  p.add(*jbytes("portion", "amount", document['portion:amount']))
+#	  p.add(*jbytes("portion", "factor", document['portion:factor']))
+#	  p.add(*jbytes("portion", "increment", document['portion:increment']))
+#	  p.add(*jbytes("portion", "multiplier", document['portion:multiplier']))
+#	  p.add(*jbytes("type", "grains", document['type:grains']))
+#	  p.add(*jbytes("type", "vegetables", document['type:vegetables']))
+#	  p.add(*jbytes("type", "fruits", document['type:fruits']))
+#	  p.add(*jbytes("type", "milk", document['type:milk']))
+#	  p.add(*jbytes("type", "meats", document['type:meats']))
+#	  p.add(*jbytes("type", "soy", document['type:soy']))
+#	  p.add(*jbytes("type", "oils", document['type:oils']))
 	  p.add(*jbytes("nutrition", "solid_fats", document['nutrition:solid_fats']))
 	  p.add(*jbytes("nutrition", "added_sugars", document['nutrition:added_sugars']))
 	  p.add(*jbytes("nutrition", "alcohol", document['nutrition:alcohol']))
