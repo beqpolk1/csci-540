6ea6a380c60996666972c459a426c3adffe8dcc4
Ben Polk <benjamin.polk@montana.edu>
2021-12-10 17:06:54 -0700

    Initial add of request handling

---
+++ knowledgeBase/src/main/java/csci/project/knowledgeBase/clientConnector/RequestRouter.java
@@ -0,0 +1,8 @@
+package csci.project.knowledgeBase.clientConnector;
+
+public class RequestRouter {
+    //posible request types: select, add, delete, search/inference
+    public RequestRouter() {
+
+    }
+}
