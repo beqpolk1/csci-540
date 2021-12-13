da3b837aa2299f53cb2aa029c530df498d10ed23
Ben P <polk.ben@gmail.com>
2021-12-12 23:07:34 -0700

    Add additional query from test agent

--- knowledgeBase/src/main/java/csci/project/knowledgeBase/testing/DemoTest.java
+++ knowledgeBase/src/main/java/csci/project/knowledgeBase/testing/DemoTest.java
@@ -1,5 +1,6 @@
 package csci.project.knowledgeBase.testing;
 
+import com.google.gson.JsonObject;
 import csci.project.knowledgeBase.backend.KbManager;
 import csci.project.knowledgeBase.client.KbClient;
 import csci.project.knowledgeBase.requests.SearchRequest;
@@ -8,13 +9,14 @@ public class DemoTest {
     public static void main(String[] args) {
         KbManager knowledgeBase = new KbManager("outdoor_gear_kb");
 
-        SimUser user1 = new SimUser(makeAgent1(), new KbClient(knowledgeBase));
+        SimUser user1 = new SimUser(makeAgent1(), new KbClient(knowledgeBase), "Bob");
         user1.runAgent();
     }
 
     private static Agent makeAgent1() {
         Agent agent = new Agent();
 
+        //query to get a gear item with a specific ID
         agent.addAction(
             (client) -> {
                 SearchRequest search = new SearchRequest("gear");
@@ -25,6 +27,39 @@ public class DemoTest {
                         if (checkVal.equals(idFilter)) return true;
                         else return false;
                     }
+                );
+
+                client.makeRequest(search);
+                System.out.println(search.getResponse().toString());
+            }
+        );
+
+        //query to get gear items that are jackets that are "insulated" type or for conditions where precipitation = "true"
+        agent.addAction(
+            (client) -> {
+                SearchRequest search = new SearchRequest("gear");
+
+                search.addCriteria(
+                        (checkObj) -> {
+                            String parTypeFilter = "jacket";
+
+                            if (!checkObj.get("par_type").isJsonNull() && !checkObj.get("par_type").getAsString().equals(parTypeFilter)) return false;
+                            else return true;
+                        }
+                );
+
+                search.addCriteria(
+                        (checkObj) -> {
+                            Boolean condPrecipFilter = true;
+                            String typeFilter = "insulated";
+
+                            if (!checkObj.get("conditions").isJsonNull()) {
+                                JsonObject conds = checkObj.get("conditions").getAsJsonObject();
+                                if (!conds.get("precip").isJsonNull() && conds.get("precip").getAsBoolean() == condPrecipFilter) return true;
+                            }
+                            if (checkObj.get("type").getAsString().equals(typeFilter)) return true;
+                            return false;
+                        }
                 );
 
                 client.makeRequest(search);
--- knowledgeBase/src/main/java/csci/project/knowledgeBase/testing/SimUser.java
+++ knowledgeBase/src/main/java/csci/project/knowledgeBase/testing/SimUser.java
@@ -5,14 +5,20 @@ import csci.project.knowledgeBase.client.KbClient;
 public class SimUser {
     private static Agent agent;
     private static KbClient client;
+    private String agentName;
 
-    public SimUser(Agent newAgent, KbClient newClient) {
+    public SimUser(Agent newAgent, KbClient newClient, String newName) {
         agent =  newAgent;
         client = newClient;
+        agentName = newName;
     }
 
     public void runAgent() {
+        int actionCnt = 0;
+
         for (TestAction curAction : agent.getActions()) {
+            actionCnt++;
+            System.out.println("-----" + agentName + " (" + actionCnt + ")-----");
             curAction.performAction(client);
         }
     }
